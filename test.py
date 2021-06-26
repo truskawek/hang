f342c26b06b9600c5d6e9763bdd939bcfda54ed1
truskawek <melzo712@gmail.com>
2021-06-26 18:50:44 +0200

    Update test.py
    -the word is shown at the end
    -using the same letter does not make you lose number of tries

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ while True:
         no_of_tries = int(input("Enter number of tries: "))
         break
     except ValueError:
-        print("Enter numeric value :)")
+        print("Enter a numeric value :)")
         
 while True:
     
@@ -41,25 +41,28 @@ while True:
     if len(letter) != 1:
         print("Enter ONE letter")
         continue
-    used_letters.append(letter)
-    found_indexes = find_indexes(word, letter)
-    if len(found_indexes) == 0:
-        print("There's no such letter in your word :(")
-        no_of_tries -= 1
-        print("Tries left: ", no_of_tries,)
-        
-        if no_of_tries == 0:
-            print("It was nice...to meet you")
-            sys.exit()
-            
+    if letter in used_letters:
+        print("You have already used that letter: " + letter)
     else:
-        for index in found_indexes:
-            user_word[index] = letter
-        whole_word = "".join(user_word)
-        if whole_word == word:
-            print("WOW, VICTORY!!! :D")
-            sys.exit()
-        print("".join(user_word))
+        used_letters.append(letter)
+        found_indexes = find_indexes(word, letter)
+        if len(found_indexes) == 0:
+            print("There's no such letter in your word :(")
+            no_of_tries -= 1
+            print("Tries left: ", no_of_tries,)
+            
+            if no_of_tries == 0:
+                print("The word was: " + word)
+                print("It was nice...to meet you")
+                sys.exit()
+                
+        else:
+            for index in found_indexes:
+                user_word[index] = letter
+            whole_word = "".join(user_word)
+            if whole_word == word:
+                print("WOW, VICTORY!!! :D")
+                sys.exit()
+            print("".join(user_word))
 
-    print("Used letters: ", used_letters )
-        
+        print("Used letters: ", used_letters )
